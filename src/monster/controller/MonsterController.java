d0c1a69d0a2629a3868c196300de58ff800801fb
kuojsean <kuo.j.sean@gmail.com>
2017-09-15 09:02:59 -0600

    Created getters and setters for the Marshmallow Monster.

--- src/monster/controller/MonsterController.java
+++ src/monster/controller/MonsterController.java
@@ -1,9 +1,12 @@
 package monster.controller;
 
+import monster.model.MarshmallowMonster;
+
 public class MonsterController
 {
 	public void start()
 	{
-		
+		MarshmallowMonster sample = new MarshmallowMonster();
+		System.out.println(sample);
 	}
 }
