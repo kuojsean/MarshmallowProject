45fc1e4fbe2b8d3d5606e10a5bde72db5c94f50d
kuojsean <kuo.j.sean@gmail.com>
2017-09-21 08:26:04 -0600

    Replaced all printlns with popup display texts or get responses.

--- src/monster/controller/MonsterController.java
+++ src/monster/controller/MonsterController.java
@@ -18,72 +18,92 @@ public class MonsterController
 	public void start()
 	{
 		MarshmallowMonster sample = new MarshmallowMonster();
-		System.out.println(sample);
-		MarshmallowMonster realMonster = new MarshmallowMonster("Dab of Ranch", 3, 4, 2.3, true);
+//		System.out.println(sample);
+		popup.displayText(sample.toString());
+		MarshmallowMonster realMonster = new MarshmallowMonster("Lavar Ball", 3, 4, 2.3, true);
 		
-		System.out.println(realMonster);
-		System.out.println("Sean is hungry, he is going to eat a tentacle");
+//		System.out.println(realMonster);
+		popup.displayText(realMonster.toString());
+//		System.out.println("Sean is hungry, he is going to eat a tentacle");
+		popup.displayText("Sean is hungry, he is going to eat a tentacle");
 		realMonster.setTentacleAmount(1.3);
-		System.out.println(realMonster);
-		
+//		System.out.println(realMonster);
+		popup.displayText(realMonster.toString());
 		interactWithTheMonster(realMonster);
 	}
 	
 	private void interactWithTheMonster(MarshmallowMonster currentMonster)
 	{
-		System.out.println(currentMonster.getName() + " wants to know what to eat next");
-		System.out.println(currentMonster.getName() + " suggests arms, they have " + currentMonster.getArmCount());
-		System.out.println("How many do you want to eat?");
+//		System.out.println(currentMonster.getName() + " wants to know what to eat next");
+		popup.displayText(currentMonster.getName() + " wants to know what to eat next");
+//		System.out.println(currentMonster.getName() + " suggests arms, they have " + currentMonster.getArmCount());
+		popup.displayText(currentMonster.getName() + " suggests arms, they have " + currentMonster.getArmCount());
+//		System.out.println("How many do you want to eat?");
+		popup.getResponse("How Mmany do you want to eat?");
 		Scanner myScanner = new Scanner(System.in);
 		int consumed = myScanner.nextInt();
 		
 		if(consumed < 0)
 		{
-			System.out.println("You cannot eat a negative amount silly human");
+//			System.out.println("You cannot eat a negative amount silly human");
+			popup.displayText("You cannot eat a negative amount silly human");
 			consumed = 0;
 		}
 		else if(consumed == 0)
 		{
-			System.out.println("Not that hungry are you?");
+//			System.out.println("Not that hungry are you?");
+			popup.displayText("Not that hungry are you?");
 		}
 		else if(consumed > currentMonster.getArmCount())
 		{
-			System.out.println("That is impossible - I only have " + currentMonster.getArmCount() + " arms!!!");
+//			System.out.println("That is impossible - I only have " + currentMonster.getArmCount() + " arms!!!");
+			popup.displayText("That is impossible - I only have " + currentMonster.getArmCount() + " arms!!!");
 		}
 		else
 		{
 			currentMonster.setArmCount(currentMonster.getArmCount() - consumed);
-			System.out.println("Thank you so much! I only have this many arms now: " + currentMonster.getArmCount());
+//			System.out.println("Thank you so much! I only have this many arms now: " + currentMonster.getArmCount());
+			popup.displayText("Thank you so much! I only have this many arms now: " + currentMonster.getArmCount());
 		}
 		
-		System.out.println(currentMonster.getName() + " wants some eyes to be zapped. He has " + currentMonster.getEyeCount() + " eyes.");
-		System.out.println("How many do you want to zap?");
+//		System.out.println(currentMonster.getName() + " wants some eyes to be zapped. He has " + currentMonster.getEyeCount() + " eyes.");
+		popup.displayText(currentMonster.getName() + " wants some eyes to be zapped. He has " + currentMonster.getEyeCount() + " eyes.");
+//		System.out.println("How many do you want to zap?");
+		popup.getResponse("How many do you want to zap?");
 		Scanner secondScanner = new Scanner(System.in);
 		int zapped = secondScanner.nextInt();
 		
 		if(zapped == 0)
 		{
-			System.out.println("Wow, you don't want to zap my eyes??");
+//			System.out.println("Wow, you don't want to zap my eyes??");
+			popup.displayText("Wow, you don't want to zap my eyes??");
 		}
 		else if (zapped < 0)
 		{
-			System.out.println("I think it's pretty obvious that you can't zap negative eyes");
+//			System.out.println("I think it's pretty obvious that you can't zap negative eyes");
+			popup.displayText("I think it's pretty obvious that you can't zap negative eyes");
 			zapped = 0;
 		}
 		else if (zapped > currentMonster.getEyeCount())
 		{
-			System.out.println("That's a little overkill but do your worst!");
+//			System.out.println("That's a little overkill but do your worst!");
+			popup.displayText("That's a little overkill but do your worst!");
 			zapped = currentMonster.getEyeCount();
 			currentMonster.setEyeCount(currentMonster.getEyeCount() - zapped);
-			System.out.println("*Zap*");
-			System.out.println("AHHHH, My eyes! I think I have " + currentMonster.getEyeCount() + " eyes!");
+//			System.out.println("*Zap*");
+			popup.displayText("*Zap*");
+//			System.out.println("AHHHH, My eyes! I think I have " + currentMonster.getEyeCount() + " eyes!");
+			popup.displayText("AHHHH, My eyes! I think I have " + currentMonster.getEyeCount() + " eyes!");
 		}
 		else
 		{
-			System.out.println("AHAHAHAAH Perfect! Zap me!");
+//			System.out.println("AHAHAHAAH Perfect! Zap me!");
+			popup.displayText("AHAHAHAAH Perfect! Zap me!");
 			currentMonster.setEyeCount(currentMonster.getEyeCount() - zapped);
-			System.out.println("*Zap*");
-			System.out.println("I only have " + currentMonster.getEyeCount() + " eyes now!");
+//			System.out.println("*Zap*");
+			popup.displayText("*Zap*");
+//			System.out.println("I only have " + currentMonster.getEyeCount() + " eyes now!");
+			popup.displayText("I only have " + currentMonster.getEyeCount() + " eyes now!");
 		}
 		
 		popup.displayText("Hey look a me!!!");
